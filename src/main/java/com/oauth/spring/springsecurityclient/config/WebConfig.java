7628873c665eb1e01ad7a3dfb96ff324bd532600
adnansee <adnansee@gmail.com>
2020-05-07 09:41:06 +0200

    Initial commit OAUTH CLIENT

---
+++ src/main/java/com/oauth/spring/springsecurityclient/config/WebConfig.java
@@ -0,0 +1,35 @@
+package com.oauth.spring.springsecurityclient.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
+
+@EnableWebMvc
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        super.addViewControllers(registry);
+        registry.addViewController("/")
+                .setViewName("forward:/index");
+
+        registry.addViewController("/index");
+        registry.addViewController("/secure");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
+    }
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer(){
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+}
---
+++ src/test/java/com/oauth/spring/springsecurityclient/SpringSecurityClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oauth.spring.springsecurityclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
